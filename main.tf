e6d67f664c86ed80e3cd713456338a033aafa398
Christopher Burnett <christopherburnett@ChristonettsMBP.edi.cloudreach.com>
2018-09-17 15:12:35 +0100

    Added Bastion security group and Elastic IP

--- main.tf
+++ main.tf
@@ -36,5 +36,26 @@ resource "aws_instance" "bastion" {
 	}
 }
 
+resource "aws_eip" "bastion_ip" {
+	instance = "${aws_instance.bastion.id}"
+}
 
+resource "aws_security_group" "bastion_security_group" {
+	name = "Bastion Security Group"
+	vpc_id = "${aws_vpc.mainVPC.id}"
+
+	ingress {
+		from_port = 0
+		to_port = 22
+		protocol = "TCP"
+		cidr_blocks = "185.73.154.30/32"
+	}
+
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = -1
+		cidr_blocks = "0.0.0.0/0"
+	}
+}
 
