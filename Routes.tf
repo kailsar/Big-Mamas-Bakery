dbd8820acc9fd99758a0c42e8f133261690e8698
Christopher Burnett <c.g.burnett@gmail.com>
2018-09-20 16:38:09 +0000

    fixed missing braces

--- Routes.tf
+++ Routes.tf
@@ -6,11 +6,13 @@ resource "aws_route_table" "private_route_table" {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_nat_gateway.my_nat_gateway.*.id[count.index]}"
   }
-  
+}
+ 
  resource "aws_route_table" "public_route_table" {
   vpc_id = "${aws_vpc.mainVPC.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.my_internet_gateway.id}"
-  } 
+  }
+}
