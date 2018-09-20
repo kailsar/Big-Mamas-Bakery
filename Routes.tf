2d008673b77e4c15789273a9e14c745f0782df59
kailsar <c.g.burnett@gmail.com>
2018-09-20 17:45:46 +0100

    Added route table associations

--- Routes.tf
+++ Routes.tf
@@ -16,3 +16,15 @@ resource "aws_route_table" "private_route_table" {
     gateway_id = "${aws_internet_gateway.my_internet_gateway.id}"
   }
 }
+
+resource "aws_route_table_association" "private_route_table" {
+  count          = "${length(var.availability_zone)}"
+  subnet_id      = "${aws_subnet.private_subnet.*.id[count.index]}"
+  route_table_id = "${aws_route_table.private_route_table.*.id[count.index]}"
+}
+
+resource "aws_route_table_association" "public_route_table" {
+  count          = "${length(var.availability_zone)}"
+  subnet_id      = "${aws_subnet.public_subnet.*.id[count.index]}"
+  route_table_id = "${aws_route_table.public_route_table.id}"
+}
