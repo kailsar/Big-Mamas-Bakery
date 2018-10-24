62517dd866b93a7088e33f91444c124d8cf934c5
Christopher Burnett <christopherburnett@ChristonettsMBP.edi.cloudreach.com>
2018-10-24 12:00:31 +0100

    ALB Target Group attachment

--- loadbalancers.tf
+++ loadbalancers.tf
@@ -29,3 +29,14 @@ resource "aws_autoscaling_attachment" "asg_attachment_alb" {
   autoscaling_group_name = "${aws_autoscaling_group.webserver_asg.id}"
   alb_target_group_arn   = "${aws_alb_target_group.webtg.arn}"
 }
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = "${aws_lb.application-lb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.webtg.arn}"
+  }
+}
